ecebffe37e42f02dbc4b50d5c22dfae3612215c1
RedJLZ <jzambrano.dev@gmail.com>
2025-05-12 23:57:31 -0500

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+// .eslintrc.cjs
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'object-curly-spacing': ['error', 'never'],
+  },
+};
